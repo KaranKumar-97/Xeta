7898e6b62f505b1e93178862e5b6cd29f6182d4b
Karan Kumar <105391077+KaranKumar-97@users.noreply.github.com>
2024-02-21 16:30:03 +0530

    Update Homepage.jsx

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,9 +5,9 @@ const Homepage = () => {
   return (
     <div className="flex justify-center items-center gap-6 h-screen w-screen">
 
-        <NavLink to="/dashboard" className="bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-xl rounded-lg px-10 py-5">Dashboard</NavLink>
+        <NavLink to="/dashboard" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl text-xl rounded-lg px-10 py-5">Dashboard</NavLink>
 
-        <NavLink to="/fitsnap" className="bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl text-xl rounded-lg px-11 py-5">Fitsnap</NavLink>
+        <NavLink to="/fitsnap" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl text-xl rounded-lg px-11 py-5">Fitsnap</NavLink>
     </div>
   )
 }
